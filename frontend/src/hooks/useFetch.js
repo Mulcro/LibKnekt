bf78cd9260e3b4264ccdc99824db75a45a5c3bce
Mulero Alamou <Mulero.alamou@gmail.com>
2024-01-08 22:03:14 -0800

    modified

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,5 +1,5 @@
 import {useContext, useEffect, useState} from 'react';
-import UserContext from './userContext';
+import UserContext from '../Context/userContext';
 import BaseUrl from '../BaseUrl';
 
 const useFetch = (ext) => {
