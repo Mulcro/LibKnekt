496843544c418ee3ecaa8a21bde915a1c67786a6
Mulero Alamou <Mulero.alamou@gmail.com>
2024-01-08 21:58:50 -0800

    created useRefresh hook to constantly send refresh requests to server

---
+++ frontend/src/hooks/useRefresh.js
@@ -0,0 +1,54 @@
+import { useContext, useEffect, useState } from "react";
+import UserContext from "./userContext";
+
+const useRefresh = (userData) => {
+
+    const [user,setUser] = useContext(UserContext);
+    
+    useEffect(() => {
+        const refreshToken = () => {
+            if(!userData) {
+                if(!localStorage.getItem("token")){
+                    return null;
+                }
+            };
+
+            const token = localStorage.getItem("token");
+
+            fetch("http://localhost:4500/refresh", {
+                headers:{
+                    "authorization": `Bearer ${token}`
+                }
+            })
+            .then(res => {
+                if(res.ok){
+                    return res.json();
+                }
+                throw new Error(res.status);
+            })
+            .then(data => {
+                sessionStorage.setItem("token",data.accessToken);
+
+                //Checks if user should still be logged
+                if(!userData){
+                    setUser(data);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+        
+        //Invoke func immedietly before running it at intervals
+        refreshToken();
+
+        const refreshTokenInterval = setInterval(() => {
+            refreshToken();
+        }, 4*60*1000);
+
+        return () => clearInterval(refreshTokenInterval);
+    },[])
+
+}
+
+export default useRefresh;
