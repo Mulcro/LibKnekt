2331227cc2fccc4a07d7dd2f96bd16290d358951
Mulero Alamou <Mulero.alamou@gmail.com>
2024-01-08 20:46:54 -0800

    refactored

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -7,9 +7,8 @@ const corsOptions = {
         } else {
             callback(new Error('Not allowed by CORS'));
         }
-    }
+    },
+    credentials: true
 };
 
 module.exports = corsOptions;
-
-// Come back to understand this
