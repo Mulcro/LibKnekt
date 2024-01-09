7a094dd17d1ad24e92a86a1760fac3c368bb93e8
Mulero Alamou <mulero.alamou@gmail.com>
2024-01-09 13:32:03 -0800

    Added heroku origin for frontend

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     "http://localhost:3000",
     "https://web.postman.co",
-    "http://lvh.me:3000"
+    "http://lvh.me:3000",
+    "https://libknekt-2b0c1dd1621b.herokuapp.com"
 ];
 
 module.exports = allowedOrigins;
